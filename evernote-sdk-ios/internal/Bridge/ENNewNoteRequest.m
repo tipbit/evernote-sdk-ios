02901c510ce6929ef7271afba70ea981b451a3e4
Ewan Mellor <ewan@tipbit.com>
2014-03-14 15:58:35 -0700

    Fix more 32/64-bit warnings.

--- evernote-sdk-ios/internal/Bridge/ENNewNoteRequest.m
+++ evernote-sdk-ios/internal/Bridge/ENNewNoteRequest.m
@@ -116,7 +116,7 @@ static NSString *kEN_NewNoteRequest_ReminderTime = @"mReminderTime";
 }
 
 - (uint32_t) totalRequestSize {
-  uint32_t result = [[self content] length];
+  uint32_t result = (uint32_t)[[self content] length];
   NSArray *attachments = self.resourceAttachments;
   for (ENResourceAttachment *anAttachment in attachments) {
     NSData *resourceData = [anAttachment resourceData];
